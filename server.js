d3c830e88280aa95b3ccae4610b1a92aecd18bd6
markcam1 <32405690+markcam1@users.noreply.github.com>
2018-07-02 18:55:00 -0700

    heroku setup

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+// var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 
 // Initialize Express
 var app = express();
